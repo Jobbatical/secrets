11ce67bcb4eb0f3ce590f0d087ca8f14ea5c0dba
Sandro Chkareuli <sandro.chqareuli1@gmail.com>
2022-05-09 17:33:08 +0300

    Add script to automatically decrypt secrets

---
+++ jobbatical.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+V=$(cat ./VERSION)
+
+source build-all
+
+cd target/$V
+
+echo "Execute file encryption using binary"
+
+SECRETS_KEY_LOCATION=global SECRETS_KEY_RING=immi-project-secrets SECRETS_REPO_HOST=github.com SECRETS_ORG=jobbatical ./secrets-darwin-amd64 open --open-all
